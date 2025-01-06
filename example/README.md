ef8167122084921d71bb09d7c4c6f871e5547d9f
Dhiraj Himani <dhiraj.himani@on-running.com>
2025-01-06 18:00:52 +0100

    -- remove example

--- example/README.md
+++
@@ -1,3 +0,0 @@
-# open_app_file_example
-
-A plug-in that can open files for android
--- example/lib/web_utils.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:html';
-
-Future<String> createTextFile(String data) async {
-  return Url.createObjectUrlFromBlob(Blob([data], 'plain/text', 'native'))
-      .toString();
-}
